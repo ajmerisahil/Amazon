3fb3ace41b05a6ef7984baff498d657b2d8384f2
Sahil Ajmeri <sahil.ms1113@gmail.com>
2021-02-27 11:47:00 +0530

    1 commit

--- mac/blog/views.py
+++ mac/blog/views.py
@@ -2,6 +2,28 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 # Create your views here.
-
 def index(request):
-    return HttpResponse('Blog Home')
+    return render(request, 'blog/index.html')
+
+def about(request):
+    return HttpResponse("We are at about")
+
+def contact(request):
+    return HttpResponse("We are at contact")
+
+def tracker(request):
+    return HttpResponse("We are at tracker")
+
+def search(request):
+    return HttpResponse("We are at search")
+
+def productView(request):
+    return HttpResponse("We are at product view")
+
+def checkout(request):
+    return HttpResponse("We are at checkout")
+
+
+#def index(request):
+#    return HttpResponse('Blog Home')
+
